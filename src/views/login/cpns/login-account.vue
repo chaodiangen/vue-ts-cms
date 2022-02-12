144aa5b15cc42a37547e600e7024bbd72e115304
diangen <chaodiangen@163.com>
2022-02-13 00:00:44 +0800

    fix(ceshi): c

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-account">
     <el-form label-width="60px" :rules="rules" :model="account" ref="formRef">
-      <el-form-item label="账号" prop="name">
+      <el-form-item label="账号1" prop="name">
         <el-input v-model="account.name" />
       </el-form-item>
       <el-form-item label="密码" prop="password">
